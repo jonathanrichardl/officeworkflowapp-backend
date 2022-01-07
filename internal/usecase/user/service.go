b14c1a42ea56575a1fc766f032a60d0a01d94a97
unknown <jonathanrichardlim@yahoo.co.id>
2022-01-07 20:37:15 +0700

    Finding bug for password change

--- internal/usecase/user/service.go
+++ internal/usecase/user/service.go
@@ -111,7 +111,7 @@ func (s *Service) ValidateAndRetrieveUser(userID string, password string) (bool,
 		return false, nil, errors.New("UserID not found")
 	}
 	sum := sha256.Sum256([]byte(password))
-	fmt.Println(sum)
+	fmt.Println(u.Password)
 	fmt.Println(fmt.Sprintf("\\x%x", sum))
 	if u.Password == fmt.Sprintf("\\x%x", sum) {
 		return true, u, nil
